46ec2ffc212350cfff71857c8a532001cd692eee
Peder Voldnes Langdal <peder.langdal@gmail.com>
2017-01-14 22:38:34 +0100

    Updated picopages installer

--- systems/picopages/installer/PicoInstall.php
+++ systems/picopages/installer/PicoInstall.php
@@ -15,32 +15,47 @@ use Composer\Installer\PackageEvent;
 
 class PicoInstaller
 {
+
     public static function postInstall(Event $event)
     {
         $composer = $event->getComposer();
-        
+
         $root_dir = __DIR__ . "/..";
 
         echo "Creating pico symlink to '/www'... \n";
         $pico_dir = $root_dir . "/vendor/picocms/pico";
         $pico_link = $root_dir . "/www";
-        symlink($pico_source, $pico_link);
+        if (!is_dir($pico_link)) {
+            symlink($pico_dir, $pico_link);
+        }
 
         echo "Creating theme and plugin symlinks... \n";
         $theme_source = $root_dir . "/vendor/pedervl/ovase-pico-theme";
         $plugin_source = $root_dir . "/vendor/pedervl/pico_edit";
-        symlink($theme_source, $pico_dir . "/themes/ovase-theme");
-        symlink($plugin_source, $pico_dir . "/plugins/pico_edit");
+        if (!is_dir($pico_dir . "/themes/ovase-theme")) {
+            symlink($theme_source, $pico_dir . "/themes/ovase-theme");
+        }
+        if (!is_dir($pico_dir . "/plugins/pico_edit")) {
+            symlink($plugin_source, $pico_dir . "/plugins/pico_edit");
+        }
 
         echo "Creating config symlinks... \n";
         $pico_cfg_source = $root_dir . "/config/config.php";
         $pico_edit_cfg_source = $root_dir . "/config/pico_edit/config.php";
-        symlink($pico_cfg_source, $pico_dir . "/config.php");
-        symlink($pico_edit_cfg_source, $plugin_source . "/config.php". 
+        if (!file_exists($pico_dir . "/config/config.php")) {
+            symlink($pico_cfg_source, $pico_dir . "/config.php");
+        }
+        if (!file_exists($plugin_source . "/config.php")) {
+            symlink($pico_edit_cfg_source, $plugin_source . "/config.php");
+        }
 
         echo "Creating content symlinks... \n";
-        symlink($root_dir . "/content", $pico_dir . "/content")
-        symlink($root_dir . "/assets", $pico_dir . "/assets")
+        if (!is_dir($pico_dir . "/content")) {
+            symlink($root_dir . "/content", $pico_dir . "/content");
+        }
+        if (!is_dir($pico_dir . "/assets")) {
+            symlink($root_dir . "/assets", $pico_dir . "/assets");
+        }
     }
 }
 
